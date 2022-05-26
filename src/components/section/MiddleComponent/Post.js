c88914aafceae46a2390734ffe957348450a10aa
Virender singh (Digvijay) <94375210+diggu13@users.noreply.github.com>
2022-05-26 12:03:06 +0530

    added post container

--- src/components/section/MiddleComponent/Post.js
+++ src/components/section/MiddleComponent/Post.js
@@ -1,8 +1,32 @@
+import sakura from '../../../Assets/cardAssets/sakura.jpg'
 import React from 'react'
 import './Post.css'
+import {BsThreeDots} from 'react-icons/bs';
+import {GiEarthAmerica} from 'react-icons/gi';
 const Post = () => {
   return (
-    <div>Post</div>
+    <div className='post-container'>
+      <div className='post-container-box'>
+        <img src={sakura} alt="post-img" className='post-img'/>
+        <div className='name-content'>
+          <span><strong>Miami Florida</strong></span>
+          <div className='time-earth'>
+            <span>Beach house </span>
+            <span>2 hrs</span>
+            <GiEarthAmerica className='earth-icon'/>
+          </div>
+        </div>
+        <BsThreeDots className='dots'/>
+      </div>
+      <p className='caption'>Need no Caption ❤️👽</p>
+      <img src={sakura} alt="sakukra" className="sakura" />
+      <hr className='hrr-line'/>
+      <div className='like-comment-share'>
+        <a href='http://localhost:3000/Facebook-clone'>Like</a>
+        <a href='http://localhost:3000/Facebook-clone'>Comment</a>
+        <a href='http://localhost:3000/Facebook-clone'>Share</a>
+      </div>
+    </div>
   )
 }
 
--- src/components/section/Middlebar.js
+++ src/components/section/Middlebar.js
@@ -3,12 +3,18 @@ import './Middlebar.css'
 import Createroom from './MiddleComponent/Createroom'
 import Newpost from './MiddleComponent/Newpost'
 import Slider from './MiddleComponent/Slider'
+import Post from './MiddleComponent/Post'
+
 const Middlebar = () => {
   return (
     <div className='middle-container'>
         <Slider />
         <Newpost />
         <Createroom/>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
     </div>
   )
 }
